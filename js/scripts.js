4a6defcf80f2a6288415252f4c54ac0b8bd25de7
cindy wambui <montanacindy5@gmail.com>
2019-09-03 12:37:27 +0300

    defined and validated pizza number input

--- js/scripts.js
+++ js/scripts.js
@@ -20,13 +20,16 @@ $(document).ready(function () {
         var pizzaFlavor = $("input:radio[name=flavor]:checked").val();
         var pizzaSize = $("input:radio[name=size]:checked").val();
         var crust = $("input:radio[name=crust]:checked").val();
+        var pizzanumber = $("input.class").val();
         if (pizzaFlavor == null) {
             alert("Please select your pizza flavor")
         } else if (pizzaSize == null) {
             alert("Please select your pizza size")
         } else if (crust == null) {
             alert("please select your crust")
+        } else if ( pizzanumber == null) {
+            alert("Please input your pizza number")
         }
-
+        
     })
 });
