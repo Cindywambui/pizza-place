fa60cdd2585fbb64473fa329a973dcfdc5856ae5
cindy wambui <montanacindy5@gmail.com>
2019-09-03 12:48:25 +0300

    define and validated delivery options and corrected pizza number input

--- js/scripts.js
+++ js/scripts.js
@@ -13,23 +13,25 @@ $(document).ready(function () {
         $("#crustsandtoppingscontent").fadeOut();
     })
     $("#delivering").click(function () {
-        $(".deliveryterms").show()    
+        $(".deliveryterms").show()
     })
     $("form#inputform").submit(function (event) {
         event.preventDefault();
         var pizzaFlavor = $("input:radio[name=flavor]:checked").val();
         var pizzaSize = $("input:radio[name=size]:checked").val();
         var crust = $("input:radio[name=crust]:checked").val();
-        var pizzanumber = $("input.class").val();
+        var pizzaNumber = $(".pizzanumber").val();
+        var delivery = $("input:radio[name=delivery]:checked").val();
         if (pizzaFlavor == null) {
             alert("Please select your pizza flavor")
         } else if (pizzaSize == null) {
             alert("Please select your pizza size")
         } else if (crust == null) {
             alert("please select your crust")
-        } else if ( pizzanumber == null) {
+        } else if (pizzaNumber == null) {
             alert("Please input your pizza number")
+        } else if (delivery == null) {
+            alert("Please select your delivery option")
         }
-        
     })
 });
